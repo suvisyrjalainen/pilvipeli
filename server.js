42761706c0369f26ecafaaecbc03fbe5855a49fa
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2019-11-22 17:47:05 +0200

    eka versio

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const app = express();
-app.listen(3000, () => console.log("kuuntelen"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("kuuntelen porttia " + port));
+
+
 app.use(express.static("public"));
